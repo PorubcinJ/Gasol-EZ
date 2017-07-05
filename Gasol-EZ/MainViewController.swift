4a4c46a04af0552510d627724a8ea85c41755a37
PorubcinJ <porubcinj@gmail.com>
2017-07-05 12:45:51 -0500

    Gasol-EZ Layout Finished (Maybe)
    Next step: Implement Google Maps Places API and Google Maps Directions API.

---
+++ Gasol-EZ/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  Gasol-EZ
+//
+//  Created by Jozef Porubcin on 7/5/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	@IBOutlet weak var gasButton: UIButton!
+	
+	@IBOutlet weak var segmentedControl: UISegmentedControl!
+}
