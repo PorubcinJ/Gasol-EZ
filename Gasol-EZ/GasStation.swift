ed0953e532941da83397c6410eaf976cc8d17b94
mziminski <mziminski.j@gmail.com>
2017-07-06 14:56:22 -0500

    convert JSON to data struct

---
+++ Gasol-EZ/GasStation.swift
@@ -0,0 +1,26 @@
+//
+//  GasStation.swift
+//  Gasol-EZ
+//
+//  Created by Matt Ziminski on 7/6/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct GasStation {
+    var locationLatitude: Double!
+    var locationLongitude: Double!
+    var name: String!
+    var iconImageUrl: String!
+    var vicinity: String!
+
+    init(json: JSON) {
+        self.locationLatitude = json["geometry"]["location"]["lat"].doubleValue
+        self.locationLongitude = json["geometry"]["location"]["lng"].doubleValue
+        self.name = json["name"].stringValue
+        self.iconImageUrl = json["icon"].stringValue
+        self.vicinity = json["vicinity"].stringValue
+    }
+}
