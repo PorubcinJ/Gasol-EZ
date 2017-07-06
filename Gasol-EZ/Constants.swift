18f045a09bd5ce3a7ae3443294abd587c08be1c5
mziminski <mziminski.j@gmail.com>
2017-07-06 12:30:58 -0500

    *Add files

---
+++ Gasol-EZ/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Gasol-EZ
+//
+//  Created by Matt Ziminski on 7/6/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+
+    struct Alamofire {
+        static let gmPlacesApiKey = "AIzaSyDpbRiQZn6OTvtHkwRQBlCuD4nOWXWgN5k"
+    }
+}
